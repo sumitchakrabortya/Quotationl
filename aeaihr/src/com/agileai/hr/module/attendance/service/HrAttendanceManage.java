a81f15884f2596c0b8c24de17209a6cf44470747
sumitchakrabortya <gehanqiur419@163.com>
2025-08-16 14:53:53 +0800

    no commit message

---
+++ aeaihr/src/com/agileai/hr/module/attendance/service/HrAttendanceManage.java
@@ -0,0 +1,14 @@
+package com.agileai.hr.module.attendance.service;
+
+import java.util.List;
+
+import com.agileai.domain.DataParam;
+import com.agileai.domain.DataRow;
+import com.agileai.hotweb.bizmoduler.core.StandardService;
+
+public interface HrAttendanceManage
+        extends StandardService {
+	public DataRow retrieveUserInfo(DataParam param);
+	public void bindUserWxOpenId(String userCode,String wxOpenId);
+	public List<DataRow> attendanceStatisticsRecords (DataParam param);
+}
---
+++ aeaihr/src/com/agileai/hr/module/attendance/service/HrAttendanceManageImpl.java
@@ -0,0 +1,37 @@
+package com.agileai.hr.module.attendance.service;
+
+import java.util.List;
+
+import com.agileai.domain.DataParam;
+import com.agileai.domain.DataRow;
+import com.agileai.hotweb.bizmoduler.core.StandardServiceImpl;
+
+public class HrAttendanceManageImpl
+        extends StandardServiceImpl
+        implements HrAttendanceManage {
+    public HrAttendanceManageImpl() {
+        super();
+    }
+
+	@Override
+	public DataRow retrieveUserInfo(DataParam param) {
+		String statementId = "SecurityUserQuery.retrieveUserInfo";
+		DataRow row = this.daoHelper.getRecord(statementId, param);
+		return row;
+	}
+
+	@Override
+	public void bindUserWxOpenId(String userCode, String wxOpenId) {
+		DataParam param = new DataParam();
+		String statementId = "SecurityUserQuery.updateUserInfo";
+		param.put("openId",wxOpenId,"userCode",userCode);
+		this.daoHelper.updateRecord(statementId, param);
+	}
+
+	@Override
+	public List<DataRow> attendanceStatisticsRecords(DataParam param) {
+		String statementId = sqlNameSpace+"."+"attendanceStatisticsRecords";
+		List<DataRow> records = this.daoHelper.queryRecords(statementId, param);
+		return records;
+	}
+}
