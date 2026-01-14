f173a202f14825f384b238e5635d26d99013488c
sumitchakrabortya <gehanqiur419@163.com>
2026-01-14 15:06:53 +0800

    no commit message

--- aeaihr/src/com/agileai/hr/bizmoduler/evection/UserListSelect.java
+++
@@ -1,7 +0,0 @@
-package com.agileai.hr.bizmoduler.evection;
-
-import com.agileai.hotweb.bizmoduler.core.PickFillModelService;
-
-public interface UserListSelect
-        extends PickFillModelService {
-}
