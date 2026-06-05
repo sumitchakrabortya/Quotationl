29b02a6af05e13e263a3072244b31b8a7633877a
sumitchakrabortya <gehanqiur419@163.com>
2026-06-05 19:14:54 +0800

    no commit message

--- aeaihr/src/com/agileai/hr/controller/MenuDataProviderHandler.java
+++ aeaihr/src/com/agileai/hr/controller/MenuDataProviderHandler.java
@@ -10,6 +10,7 @@ import org.codehaus.jettison.json.JSONObject;
 
 import com.agileai.domain.DataParam;
 import com.agileai.domain.DataRow;
+import com.agileai.hotweb.bizmoduler.frame.FunctionManage;
 import com.agileai.hotweb.bizmoduler.frame.SecurityAuthorizationConfig;
 import com.agileai.hotweb.common.BeanFactory;
 import com.agileai.hotweb.controller.core.BaseHandler;
@@ -20,13 +21,15 @@ import com.agileai.hotweb.domain.core.User;
 import com.agileai.hotweb.domain.system.FuncMenu;
 import com.agileai.hotweb.renders.AjaxRenderer;
 import com.agileai.hotweb.renders.ViewRenderer;
-import com.agileai.hr.cxmodule.FunctionTreeManage;
+
 
 public class MenuDataProviderHandler extends BaseHandler{
 	
 	public ViewRenderer prepareDisplay(DataParam param){
 		String userId = param.get("userId");
-		SecurityAuthorizationConfig authorizationConfig = (SecurityAuthorizationConfig)this.lookupService("securityAuthorizationConfigService");
+		String folderId = param.get("folderId"); 
+		SecurityAuthorizationConfig authorizationConfig = 
+				(SecurityAuthorizationConfig)this.lookupService("securityAuthorizationConfigService");
 		DataRow userRow = authorizationConfig.retrieveUserRecord(userId);
 		User user = new User();
 		this.initAuthedUser(userRow,user);
@@ -35,67 +38,78 @@ public class MenuDataProviderHandler extends BaseHandler{
 		TreeBuilder treeBuilder = new TreeBuilder(funcRecords,"funcId","funcName","funcPid");
 		treeBuilder.setTypeKey("funcType");
 		treeBuilder.setPojoList(true);
-        TreeModel treeModel = treeBuilder.buildTreeModel();
-        List<TreeModel> treeModels = treeModel.getChildren();
-        JSONObject jsonObject = new JSONObject();        
-        try {
-    		JSONArray jsonArray = new JSONArray();
-    		jsonObject.put("menus", jsonArray);
-    		
-    		for (int i=0;i < treeModels.size();i++) {
-    			TreeModel topChildModel = treeModels.get(i);
-    			JSONObject topJsonChild = new JSONObject();
-    			jsonArray.put(topJsonChild);
-    			buildJsonObjects(topChildModel,topJsonChild);
-    		}
+		treeBuilder.setRootId(folderId);
+		TreeModel treeModel = treeBuilder.buildTreeModel();
+		List<TreeModel> treeModels = treeModel.getChildren();
+		JSONObject jsonObject = new JSONObject();
+		try {
+			JSONArray jsonArray = new JSONArray();
+			jsonObject.put("menus", jsonArray);
+			
+//			BeanFactory beanFactory = BeanFactory.instance();
+//			AppConfig appConfig = beanFactory.getAppConfig();
+//			String menuURLPrefix = appConfig.getConfig("GlobalConfig","MenuURlPrefix");
+			String menuURLPrefix = "http://localhost:6060/aeaihr/";
+			
+			for (int i = 0; i < treeModels.size(); i++) {
+				TreeModel topChildModel = treeModels.get(i);
+				JSONObject topJsonChild = new JSONObject();
+				jsonArray.put(topJsonChild);
+				buildJsonObjects(topChildModel,topJsonChild,menuURLPrefix);
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-        String responseText = jsonObject.toString();
+		String responseText = jsonObject.toString();
 		return new AjaxRenderer(responseText);
 	}
-	
-	public void buildJsonObjects(TreeModel treeModel,JSONObject jsonObject) throws JSONException {
-		jsonObject.put("id", treeModel.getId());
-    	jsonObject.put("text", treeModel.getName());
-    	jsonObject.put("url", treeModel.getProperty().get("funcUrl"));
-    	jsonObject.put("height", "");
+
+	private void buildJsonObjects(TreeModel treeModel,
+			JSONObject jsonObject, String menuURLPrefix)throws JSONException {
+		jsonObject.put("id",treeModel.getId());
+		jsonObject.put("text", treeModel.getName());
+		jsonObject.put("url",menuURLPrefix + treeModel.getProperty().get("funcUrl"));
+		jsonObject.put("height", "");
 		
 		List<TreeModel> children = treeModel.getChildren();
-        if (!children.isEmpty()) {
-    		JSONArray jsonArray = new JSONArray();
-    		jsonObject.put("menus", jsonArray);
-            for (TreeModel child : children) {
-            	JSONObject jsonChild = new JSONObject();
-            	jsonArray.put(jsonChild);
-            	buildJsonObjects(child,jsonChild);
-            }  
-        }
+		if(!children.isEmpty()){
+			JSONArray jsonArray = new JSONArray();
+			jsonObject.put("menus", jsonArray);
+			for(TreeModel child : children){
+				JSONObject jsonChild = new JSONObject();
+				jsonArray.put(jsonChild);
+				buildJsonObjects(child,jsonChild,menuURLPrefix);
+			}
+		}
+		
+		
 	}
-	
-	private List<FuncMenu> getAuthedFuncList(User user){
+
+	private List<FuncMenu> getAuthedFuncList(User user) {
 		List<FuncMenu> result = null;
-		FunctionTreeManage functionTreeManage = (FunctionTreeManage)this.lookupService("functionTreeManageService");
-		if (user.isAdmin()){
+		FunctionManage functionTreeManage = (FunctionManage)this.lookupService("functionTreeManageService");
+		if(user.isAdmin()){
 			result = functionTreeManage.getFuncMenuList();
 		}else{
 			List<FuncMenu> funcs = new ArrayList<FuncMenu>();
 			List<FuncMenu> funcList = functionTreeManage.getFuncMenuList();
-			for (int i=0;i < funcList.size();i++){
+			for (int i = 0; i < funcList.size(); i++) {
 				FuncMenu function = funcList.get(i);
 				String functionId = function.getFuncId();
-				if (user.containResouce(Resource.Type.Menu, functionId)){
-					funcs.add(function);						
+				if (user.containResouce(Resource.Type.Menu, functionId)) {
+					funcs.add(function);
 				}
 			}
-			result = funcs;			
+			result = funcs;
 		}
 		return result;
 	}
-	
-	public void initAuthedUser(DataRow userRow,User user){
+
+	private void initAuthedUser(DataRow userRow, User user) {
 		BeanFactory beanFactory = BeanFactory.instance();
-		SecurityAuthorizationConfig authorizationConfig = (SecurityAuthorizationConfig)beanFactory.getBean("securityAuthorizationConfigService");
+		SecurityAuthorizationConfig authorizationConfig = 
+				(SecurityAuthorizationConfig)beanFactory.getBean("securityAuthorizationConfigService");
 		String userId = userRow.stringValue("USER_ID");
 		String userCode = userRow.stringValue("USER_CODE");
 		String userName = userRow.stringValue("USER_NAME");
@@ -103,14 +117,14 @@ public class MenuDataProviderHandler extends BaseHandler{
 		user.setUserCode(userCode);
 		user.setUserName(userName);
 		
-		FunctionTreeManage functionTreeManage = (FunctionTreeManage)beanFactory.getBean("functionTreeManageService");
+		FunctionManage functionTreeManage = (FunctionManage)beanFactory.getBean("functionTreeManageService");
 		List<String> menuItemIdList = authorizationConfig.retrieveMenuIdList(userId);
 		
-		if (menuItemIdList != null){
-			if (menuItemIdList.contains(FuncMenu.RootId)){
+		if (menuItemIdList != null) {
+			if (menuItemIdList.contains(FuncMenu.RootId)) {
 				String functionId = FuncMenu.RootId;
 				FuncMenu funcMenu = functionTreeManage.getFunction(functionId);
-				HashMap<String,Resource> menuResources = getResourceRef(user, Resource.Type.Menu);
+				HashMap<String,Resource> menuResources = getResourceRef(user,Resource.Type.Menu);
 				
 				Resource resource = new Resource();
 				resource.setResouceType(Resource.Type.Menu);
@@ -120,50 +134,56 @@ public class MenuDataProviderHandler extends BaseHandler{
 				
 				List<FuncMenu> children = funcMenu.getChildren();
 				this.processFuncMenu(user,children,menuItemIdList);
+				
 			}
 		}
 	}
-	
-	private void processFuncMenu(User user,List<FuncMenu> funcMenuList
-			,List<String> funcMenuIdList){
-		for (int i=0;i < funcMenuList.size();i++){
+
+	private void processFuncMenu(User user, List<FuncMenu> funcMenuList,
+			List<String> funcMenuIdList) {
+		for (int i = 0; i < funcMenuList.size(); i++) {
 			FuncMenu funcMenu = funcMenuList.get(i);
 			String curFuncId = funcMenu.getFuncId();
-			if (funcMenuIdList.contains(curFuncId)){
-				HashMap<String,Resource> menuResources = getResourceRef(user, Resource.Type.Menu);
+			if(funcMenuList.contains(curFuncId)){
+				HashMap<String,Resource> menuResources = getResourceRef(user,Resource.Type.Menu);
 				Resource menuResource = new Resource();
 				menuResource.setResouceType(Resource.Type.Menu);
 				String menuId = funcMenu.getFuncId();
 				menuResource.setResouceId(menuId);
 				menuResource.setResouceName(funcMenu.getFuncName());
 				menuResources.put(menuId, menuResource);
+				
 			}else{
-				loopFuncMenuList(funcMenu,funcMenuList, funcMenuIdList, user);
+				loopFuncMenuList(funcMenu,funcMenuList,funcMenuIdList,user);
+				
 			}
 			
-			if (user.containResouce(Resource.Type.Menu, curFuncId)){
-				if (funcMenu.isFolder()){
+			if (user.containResouce(Resource.Type.Menu, curFuncId)) {
+				if (funcMenu.isFolder()) {
 					List<FuncMenu> curFuncMenuList = funcMenu.getChildren();
-					if (curFuncMenuList.size() > 0){
-						this.processFuncMenu(user,curFuncMenuList,funcMenuIdList);						
+					if (curFuncMenuList.size()>0) {
+						this.processFuncMenu(user, curFuncMenuList, funcMenuIdList);
 					}
 				}
+				
 			}
 		}
 	}
-	
-	private void loopFuncMenuList(FuncMenu curMenuItem,List<FuncMenu> funcMenuList,List<String> funcMenuIdList,User user){
-		boolean isMatched = false;
-		for (int i=0;i < funcMenuList.size();i++){
+
+	private void loopFuncMenuList(FuncMenu curMenuItem,
+			List<FuncMenu> funcMenuList, List<String> funcMenuIdList, User user) {
+		boolean isMatched =false;
+		for (int i = 0; i < funcMenuList.size(); i++) {
 			FuncMenu menuItem = funcMenuList.get(i);
 			String menuItemId = menuItem.getFuncId();
-			if (funcMenuIdList.contains(menuItemId)){
+			if (funcMenuIdList.contains(menuItemId)) {
 				isMatched = true;
 				break;
 			}
 		}
-		if (!isMatched){
-			HashMap<String,Resource> menuResources = getResourceRef(user, Resource.Type.Menu);
+		
+		if (!isMatched) {
+			HashMap<String,Resource> menuResources = getResourceRef(user,Resource.Type.Menu);
 			Resource menuResource = new Resource();
 			menuResource.setResouceType(Resource.Type.Menu);
 			String menuId = curMenuItem.getFuncId();
@@ -171,14 +191,17 @@ public class MenuDataProviderHandler extends BaseHandler{
 			menuResource.setResouceName(curMenuItem.getFuncName());
 			menuResources.put(menuId, menuResource);
 		}
+		
 	}
-	private HashMap<String,Resource> getResourceRef(User user,String resourceType){
-		HashMap<String,Resource> result = null;
-		if (!user.getResoucesRefs().containsKey(resourceType)){
-			HashMap<String,Resource> temp = new HashMap<String,Resource>();
+
+	private HashMap<String, Resource> getResourceRef(User user, String resourceType) {
+		HashMap<String, Resource> result = null;
+		if(!user.getResoucesRefs().containsKey(resourceType)){
+			HashMap<String, Resource> temp = new HashMap<String, Resource>();
 			user.getResoucesRefs().put(resourceType, temp);
 		}
 		result = user.getResoucesRefs().get(resourceType);
 		return result;
 	}
+
 }
